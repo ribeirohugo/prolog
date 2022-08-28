f4ebb042f0df8a89100f6dd8edd04318d5b8c058
Hugo Ribeiro <ribeirohugo@outlook.pt>
2022-08-28 17:32:09 +0100

    Adicionados predicados em Prolog.

---
+++ predicados.pl
@@ -0,0 +1,33 @@
+% Cálculo da média
+
+media([],0):-!.
+media(L,M):-soma_media(L,S,C), M is S/C.
+
+soma_media([],0,0).
+soma_media([X|L],S,C):-soma_media(L,S1,C1), S is S1+X, C is C1+1.
+
+% Obter valor mínimo de lista de inteiros
+
+menor([X],X):-!.
+menor([X,Y|L],M):- X > Y -> menor([X|L],M); menor([Y|L],M).
+
+minimo([X],X):-!.
+minimo([X,Y|L],M):- X > Y, !, minimo([Y|L],M).
+minimo([X,_|L],M):- minimo([X|L],M).
+
+% Obter valor máximo de lista de inteiros
+
+maior([X],X):-!.
+maior([X,Y|L],M):- X > Y, !, maior([X|L],M).
+maior([_,X|L],M):- maior([X|L],M).
+
+% Contar o valor de pares e ímpares de uma lista de inteiros
+
+contar_pares_impares([],0,0).
+contar_pares_impares([X|L],P,I):- 0 is mod(X, 2), !, contar_pares_impares(L,P1,I), P is P1+1.
+contar_pares_impares([_|L],P,I):-contar_pares_impares(L,P,I1), I is I1+1.
+
+% Verificar se lista tem repetidos
+
+tem_repetidos([X,Y|_]):-X == Y,!.
+tem_repetidos([_|L]):-tem_repetidos(L).
