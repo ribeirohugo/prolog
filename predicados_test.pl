22e461351e5e377d9d5af3bc94e6fee9303d6f6c
Hugo Ribeiro <ribeirohugo@outlook.pt>
2022-08-29 15:03:07 +0100

    test: adicionados testes para os predicados maior, contar_pares_impares e tem_repetidos

--- predicados_test.pl
+++ predicados_test.pl
@@ -4,7 +4,6 @@
 :- consult("predicados.pl").
 
 % media
-
 test(media) :-
         media([2,2], 2).
 
@@ -12,11 +11,35 @@ test(media) :-
         media([-10,10], 0).
 
 % menor
-
 test(menor) :-
         menor([2,3,4,1], 1).
 
 test(minimo) :-
         minimo([2,3,4,1], 1).
+
+% maior
+test(maior) :-
+        maior([2,3,4,1], 4).
+
+test(maior) :-
+        maior([-2,-3,-4,-1], -1).
+
+% contar_pares_impares
+test(contar_pares_impares) :-
+        contar_pares_impares([1,2,3,4,5], 2, 3).
+
+test(contar_pares_impares) :-
+        contar_pares_impares([-2,-3,-4,-1], 2, 2).
+
+% tem_repetidos
+test(tem_repetidos) :-
+        tem_repetidos([1,2,3,4,1]).
+
+test(tem_repetidos) :-
+        tem_repetidos([1,2,3,4,4]).
+
+test(tem_repetidos, [fail]) :-
+        tem_repetidos([1,2,3,4,5]).
+
 
 :- end_tests(lists).
