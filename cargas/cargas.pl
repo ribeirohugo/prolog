e8e5eea33ab45cd85d8febfff310f3512d995d55
Hugo Ribeiro <ribeirohugo@outlook.pt>
2022-09-03 17:20:03 +0100

    fix: actualizada descrição do predicado "seq_custo_min".

--- cargas/cargas.pl
+++ cargas/cargas.pl
@@ -33,28 +33,8 @@ custo([X,Y|LCidades],[P|LPesos],Custo):-
     custo([Y|LCidades],LPesos,C),
     Custo is C+(Dist*P).
 
-% d)[30%] Escreva um predicado seq_custo_min(LC,Custo) que determine a sequência de entregas de todas as
-  %cargas que leva ao menor custo de acordo com o cálculo do custo definido na alínea anterior, retornando em
-  %LC a sequência de cidades no trajeto de entregas e em Custo o respetivo custo. As cidades a entregar são
-  %aquelas que estiverem nos factos do tipo carga/2.
-  %Sugestões:
-  %-se obtiver uma lista com todas as cidades pode gerar todas as sequências de cidades através da permutação
-  %de elementos da lista e para o efeito pode usar o predicado já existente no SWI Prolog
-  %permutation(ListaCidades,ListaCidadesPermutada) que terá a capacidade de gerar todas as permutações de
-  %ListaCidades. Desse modo não precisará aplicar um método de geração de caminhos visto que o
-  %permutation/2 tem essa capacidade. O predicado permutation/2 funciona do seguinte modo:
-  %?- permutation([1,2,3],L).
-  %L = [1, 2, 3] ;
-  %L = [1, 3, 2] ;
-  %L = [2, 1, 3] ;
-  %L = [2, 3, 1] ;
-  %L = [3, 1, 2] ;
-  %L = [3, 2, 1] ;
-  %false.
-  %-guarde num facto dinâmico custo_min(ListaCidadesPermutada,Custo) o menor Custo da melhor lista de
-  %cidades permutada ListaCidadesPermutada enquanto os custos das diversas permutações de cidades estão a
-  %ser calculados, no final o facto terá a melhor solução.
-
+% Predicado que determina a sequência de entregas de todas as cargas que leva ao menor custo de acordo com o cálculo do
+% custo, retornando a sequência de cidades no trajeto de entregas e em Custo o respetivo custo.
 seq_custo_min(LC,Custo):-(run;true),custo_min(LC,Custo).
 
 run:-
